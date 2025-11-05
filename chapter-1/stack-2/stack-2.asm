5a84e65d04ab0900316d0f204830f6b4dec2a9c1
Amith <amithajith7025@gmail.com>
2025-11-06 05:17:47 +0530

    memorized assembly after a long time

---
+++ chapter-1/stack-2/stack-2.asm
@@ -0,0 +1,57 @@
+; stack.asm
+extern printf
+
+section .data
+	string		db		"ABCDE", 0
+	stringLen	equ		$ - string - 1
+	fmt1		db		"The original string: %s", 10, 0
+	fmt2		db		"The reversed string: %s", 10, 0
+
+section .bss
+section .text
+	global main
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	;print the original string
+
+	mov rax, 0
+	mov rdi, fmt1
+	mov rsi, string
+
+	call printf
+
+	;push the string char per char on the stack
+	xor rax, rax
+	mov rbx, string
+	mov rcx, stringLen
+	mov r12, 0
+	
+	pushLoop:
+		mov al, byte [rbx + r12]
+		push rax
+		inc r12
+		loop pushLoop
+
+	;Now lets reverse it
+
+	mov rbx, string
+	mov rcx, stringLen
+	mov r12, 0
+	popLoop:
+		pop rax
+		mov byte [rbx + r12], al
+		inc r12
+		loop popLoop
+	mov byte [rbx + r12], 0
+
+	mov rdi, fmt2
+	mov rsi, string
+	mov rax, 0
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	ret
